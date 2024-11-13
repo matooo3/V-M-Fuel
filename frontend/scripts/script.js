373a1dbe7cf0913d4b53b275c13bbf38f13c3386
matooo3 <matze2948@gmail.com>
2024-11-13 23:42:39 +0100

    added homeTab
    added homeTab

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -14,7 +14,7 @@ function router() {
     const hash = window.location.hash.slice(1);
     // Fallback auf Home-Seite
     const baseTab = "initial Page to implement";
-    const loadPage = routes[hash] || baseTab;
+    const loadPage = routes[hash]() || baseTab;
     // Löscht vorherigen Inhalt
     // document.getElementById('app').innerHTML = '';
     loadPage();
@@ -24,3 +24,7 @@ function router() {
 window.addEventListener('hashchange', router);
 // Lädt die Standardseite beim Start:
 window.addEventListener('load', router);
+
+
+
+
