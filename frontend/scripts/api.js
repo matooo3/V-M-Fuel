af87fc9e9d8b4bf4873b748ce8fb6b9e61d1b336
matooo3 <matze2948@gmail.com>
2025-05-20 14:54:20 +0200

    reverse proxy apiBaseUrl
    reverse proxy apiBaseUrl

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -1,5 +1,6 @@
 // BACKEND base-URL
-const apiBaseUrl = 'http://172.18.45.1:3000';
+// const apiBaseUrl = 'http://172.18.45.1:3000'; // DIREKT
+const apiBaseUrl = 'https://gfoh.ddns.net:6969'; // PER REVERSE PROXY
 // const apiBaseUrl = `${window.location.protocol}//${window.location.hostname}:3000`;
 
 // fetching any DATA-ENDPOINT from BACKEND
