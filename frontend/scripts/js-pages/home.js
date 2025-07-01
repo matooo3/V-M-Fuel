19dde00bed458a07000797956f4bf70a8b22f5da
Daniel Mehler <127515405+Wichkey@users.noreply.github.com>
2025-07-01 14:42:11 +0200

    Updated responsiveness of dashoboard

--- frontend/scripts/js-pages/home.js
+++ frontend/scripts/js-pages/home.js
@@ -7,7 +7,7 @@ export default async function loadHome() {
     // LOAD app html-code
     const html = await loadHTMLTemplate('/frontend/html-pages/home.html');
     app.innerHTML = html;
-    // updateAdminContainer();
+    updateAdminContainer();
 
     function updateAdminContainer() {
         const activeTab = document.querySelector('.tab.active');
@@ -78,20 +78,84 @@ export default async function loadHome() {
                             <span class="subtext">Chili con carne</span>
                         </div>
                         <h3 id="todays-calories">820ckal</h3>
-                    </div>
-                    <div class="card drop-shadow mealcards">
-                        <div class="red-point"></div>
-                        <div id="todays-meal-info">
-                            <h3 class="meal-name">Lunch</h3>
-                            <span class="subtext">Chili con carne</span>
-                        </div>
-                        <h3 id="todays-calories">820ckal</h3>
-                    </div>
+                    </div>                                  
                 </div>
             </div>
         </div>`;
         } else {
-            adminContainer.innerHTML = '';
+            adminContainer.innerHTML = `
+            <div id="user-managment-container">
+            <div id="user-info">
+                <h1 id="user-text">User management</h1>
+                <div id="user-amount">
+                    <span class="subtext meals-amount-text">Total users</span>
+                    <div id="user-red-point"></div>
+                    <span class="subtext meals-amount-text">500</span>
+                </div>
+                <div id="search-bar">
+                    <img id="search-icon" src="/frontend/assets/icons/search-icon.png" alt="search icon">
+                    <input type="text" placeholder="Search User">
+                </div>
+            </div>
+            <div id="user-list">
+                <div class="card user">
+                    <div class="profile-picture">
+                        <span>DM</span>
+                    </div>
+                    <div class="user-data">
+                        <span class="user-name">Daniel Mehler</span>
+                        <span class="user-email">daniel.mehler@gmail.com</span>
+                    </div>
+                    <div id="user-role">
+                        <div class="user-tag">
+                            <img class="user-tag-logo" src="/frontend/assets/icons/admin-tag.png" alt="tag">
+                            <span class="user-tag-text">Admin</span>
+                        </div>
+                        <img id="change-role" src="/frontend/assets/icons/change-role.png" alt="change role">
+                    </div>
+                </div>
+                <div class="card user">
+                    <div class="profile-picture">
+                        <span>DM</span>
+                    </div>
+                    <div class="user-data">
+                        <span class="user-name">Daniel Mehler</span>
+                        <span class="user-email">daniel.mehler@gmail.com</span>
+                    </div>
+                    <div id="user-role">
+                        <div class="user-tag">
+                            <img class="user-tag-logo" src="/frontend/assets/icons/admin-tag.png" alt="tag">
+                            <span class="user-tag-text">Admin</span>
+                        </div>
+                        <img id="change-role" src="/frontend/assets/icons/change-role.png" alt="change role">
+                    </div>
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+                <div class="card user">
+                </div>
+            </div>
+        </div>`;
         }
     }
 
@@ -111,19 +175,6 @@ export default async function loadHome() {
             updateAdminContainer();
 
         });
-    });
-
-    const mealsInfo = document.getElementById('meals-info');
-    const nextMealText = document.getElementById('next-meal-text');
-
-    mealsInfo.addEventListener('scroll', () => {
-        if (mealsInfo.scrollTop > 0) {
-            // Gescrollt - Margin entfernen
-            nextMealText.style.marginBottom = '0px';
-        } else {
-            // Ganz oben - Original Margin wiederherstellen
-            nextMealText.style.marginBottom = '20px'; // Dein ursprünglicher Wert
-        }
     });
 
 
