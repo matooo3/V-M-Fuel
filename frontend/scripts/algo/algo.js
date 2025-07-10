ea43bb57db53a4852549ffa46f66412c0b55b76d
matooo3 <matze2948@gmail.com>
2025-07-10 20:36:59 +0200

    Create algo.js

---
+++ frontend/scripts/algo/algo.js
@@ -0,0 +1,51 @@
+export function algo(kcal, puffer, like, dislike) {
+    // ...
+
+    array = scale(kcal);
+
+    const scaledBFkcal = array["Breakfast"];
+    const scaledLUkcal = array["Lunch"];
+    const scaledDIkcal = array["Dinner"];
+    const scaledPUkcal = array["Puffer"];
+
+    for(let i = 0; i < 7; i++) {
+        createDay(kcal);
+    }
+    
+}
+
+export function createDay(scaledBDkcal, scaledLUkcal, scaledDIkcal, scaledPUkcal) {
+    searchMealForBreakfast(scaledBDkcal); // => DB (MIN/MAX)
+    searchMealForLunch(scaledLUkcal);
+    searchMealForDinner(scaledDIkcal);
+    searchMealForPuffer(scaledPUkcal);
+}
+
+export function scale(kcal) {
+    // ........
+    // 24% / 38% / 38%
+    // 20%  / 35% / 35% / 10% 
+    // 17%  / 32% / 32% / 19%
+    const array = [];
+    return array;
+}
+
+export function searchMealForBreakfast(kcal) {
+    // ...
+    calcPuffer();
+}
+
+export function searchMealForLunch(kcal) {
+    // ...
+    calcPuffer();
+}
+
+export function searchMealForDinner(kcal) {
+    // ...
+    calcPuffer();
+}
+
+export function searchMealForPuffer(kcal) {
+    // ...
+    calcPuffer();
+}
